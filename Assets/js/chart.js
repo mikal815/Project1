63dfe64789e3f2826a98deb1ed41fa042f4b57a0
Mike M <mikal815@users.noreply.github.com>
2018-07-03 18:29:02 -0400

    added pie chart

--- Assets/js/chart.js
+++ Assets/js/chart.js
@@ -26,7 +26,22 @@ $.ajax({
             labels: ["Bitcoin", "Bitcoin Cash", "Mixin", "Maker", "Ethereum"],
             datasets: [{
                 label: "Price USD",
-                data: [response.data["1"]["quotes"]["USD"]["price"], response.data["1831"]["quotes"]["USD"]["price"], response.data["2349"]["quotes"]["USD"]["price"], response.data["1518"]["quotes"]["USD"]["price"], response.data["1027"]["quotes"]["USD"]["price"]]
+                data: [response.data["1"]["quotes"]["USD"]["price"], response.data["1831"]["quotes"]["USD"]["price"], response.data["2349"]["quotes"]["USD"]["price"], response.data["1518"]["quotes"]["USD"]["price"], response.data["1027"]["quotes"]["USD"]["price"]],
+                backgroundColor: ["rgb(95, 150, 79)", "rgb(251, 255, 211)", "rgb(201, 229, 192)", "rgb(221, 219, 106)", "rgb(118, 170, 117)"]
+            }]
+        },
+        options: {}
+    });
+
+    var myChart2 = document.getElementById('myChart2').getContext('2d');
+    var coinCompareChart2 = new Chart(myChart2, {
+        type: 'pie',
+        data: {
+            labels: ["Bitcoin", "Bitcoin Cash", "Mixin", "Maker", "Ethereum"],
+            datasets: [{
+                label: "Price USD",
+                data: [response.data["1"]["quotes"]["USD"]["price"], response.data["1831"]["quotes"]["USD"]["price"], response.data["2349"]["quotes"]["USD"]["price"], response.data["1518"]["quotes"]["USD"]["price"], response.data["1027"]["quotes"]["USD"]["price"]],
+                backgroundColor: ["rgb(95, 150, 79)", "rgb(251, 255, 211)", "rgb(201, 229, 192)", "rgb(221, 219, 106)", "rgb(118, 170, 117)"]
             }]
         },
         options: {}
@@ -35,6 +50,24 @@ $.ajax({
 
 
 
+
 });
+
+
+// 2nd api
+
+$.ajax({
+    //url: queryURL,
+    url: "https://api.coinranking.com/v1/public/coin/1335/history/7d?base=USD",
+    method: "GET"
+ }).then(function (response) {
+    var coinName1 = "Bitcoin";
+
+    console.log(response.data, 'RESPONSE.DATA.');
+    console.log(coinName1);
+    console.log(response.data.change);
+    console.log(response.data.history[290]);
+ 
+ });
 
 
